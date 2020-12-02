ad2c28ddf5a84ea17a417ccc339d2c7a15eb4ba8
Michele Byman <michelebyman@gmail.com>
2020-12-02 10:37:44 +0100

    Initial Commit

---
+++ dropdown menu/dropdown_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropdown_menuApp.swift
+//  dropdown menu
+//
+//  Created by Michele Byman on 2020-12-02.
+//
+
+import SwiftUI
+
+@main
+struct dropdown_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
