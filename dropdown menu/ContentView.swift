9fbed71f8f6de373f7a7a76ef734bdaea9062936
Michele Byman <michelebyman@gmail.com>
2020-12-02 12:58:13 +0100

    drop down menu with animation

--- dropdown menu/ContentView.swift
+++ dropdown menu/ContentView.swift
@@ -8,10 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOpen = false
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+    
+            VStack {
+      
+            
+                Button(action: {isOpen.toggle()}) {
+                    Text("Menu")
+                        .padding([.top, .leading, .bottom])
+                    Image(systemName: isOpen ?  "chevron.down" :  "chevron.up")
+                        .padding(.trailing)
+                }
+                if isOpen {
+                NavigationLink(destination: Text("Item 1")) {
+                    Text("Item 1").padding()
+                    
+                }
+                NavigationLink(destination: Text("Item 2")) {
+                    Text("Item 2").padding()
+                }
+                NavigationLink(destination: Text("Item 3")) {
+                    Text("Item 3").padding()
+                }
+                NavigationLink(destination: Text("Item 3")) {
+                    Text("Item 4").padding()
+                }
+                }
+               
+            }
             .padding()
-    }
+            .background(Color(.black))
+            .cornerRadius(6)
+            .animation(.spring())
+            
+            }
+           
+        }
+        
+     
 }
 
 struct ContentView_Previews: PreviewProvider {
